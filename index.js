ff2b9911797ea4b2fbec37c842a4cd029ac8950b
Biruk Lemma <biruklemmadebela@gmail.com>
2024-06-26 11:54:28 +0300

    minor

--- index.js
+++ index.js
@@ -302,7 +302,7 @@ const upload = multer({ storage });
 
 // Enable CORS for all routes
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://song-app-frontend.vercel.app'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204
